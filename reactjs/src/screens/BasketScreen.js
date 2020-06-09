5121d89245f67ec777b366e59120a01b6901b00b
samirazazy <samiralazazy@gmail.com>
2020-06-09 09:26:05 +0200

    refactor busjet screen

--- reactjs/src/screens/BasketScreen.js
+++ reactjs/src/screens/BasketScreen.js
@@ -1,127 +1,134 @@
-import React, { useEffect ,useState } from "react";
+import React, { useEffect, useState } from "react";
 import { addToBasket, deleteFromBasket } from "../functions/basketActions";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 function BasketScreen(props) {
-
   const basket = useSelector((state) => state.basket);
 
   const { basketItems } = basket;
 
-  
-
   const itemId = props.match.params.id;
-  let quantaty  = props.location.search
+  let quantaty = props.location.search
     ? Number(props.location.search.split("=")[1])
     : 1;
   const dispatch = useDispatch();
   const [count, setCount] = useState(quantaty);
   const deleteItem = (itemId) => {
     dispatch(deleteFromBasket(itemId));
-    console.log('Deleting')
+    console.log("Deleting");
   };
 
   useEffect(() => {
-    
     if (itemId) {
-      dispatch(addToBasket(itemId, count ));
+      dispatch(addToBasket(itemId, count));
     }
-  }, [dispatch,itemId,count]);
+  }, [dispatch, itemId, count]);
 
   const checkout = () => {
     // props.history.push("/");
   };
 
-
   return (
     <div className="basket">
       <div className="basketList">
         <ul className="basketListContainer">
-          <li className="shoppingCart">
-            Shopping Cart
-          </li>
-          {basketItems.length === 0 ? 
+          <li className="shoppingCart">Shopping Cart</li>
+          {basketItems.length === 0 ? (
             <div className="emptyCart">
               Your Shopping Cart is empty...
-              
               <Link to="/">Go Shopping</Link>
             </div>
-           : 
-          
+          ) : (
             basketItems.map((item) => (
               <li key={item.product}>
                 <div className="basketImage">
-                    <Link  to={"/item/" + item.product}>
-                      <img src={item.image} alt="product" />
-                    </Link>
-                  </div>
+                  <Link to={"/item/" + item.product}>
+                    <img src={item.image} alt="product" />
+                  </Link>
+                </div>
 
                 <div className="basketName">
-                      <Link className="itemName" to={"/item/" + item.product}>{item.name}</Link>
-                      <div>{item.description}</div>
-                  </div>
+                  <Link className="itemName" to={"/item/" + item.product}>
+                    {item.name}
+                  </Link>
+                  <div>{item.description}</div>
+                </div>
 
                 <div className="basketQuantaty ">
-                  <button className="quantatyButton" onClick={() =>{ setCount(count - 1)}} > &#x2796; </button>
+                  <button
+                    className="quantatyButton"
+                    onClick={() => {
+                      setCount(count - 1);
+                    }}
+                  >
+                    &#x2796;
+                  </button>
                   <p className="quantatyNumber">{count}</p>
-                  <button className="quantatyButton" onClick={() =>{ setCount(count + 1)}} > &#x2795; </button>
+                  <button
+                    className="quantatyButton"
+                    onClick={() => {
+                      setCount(count + 1);
+                    }}
+                  >
+                    &#x2795;
+                  </button>
                 </div>
 
                 <div className="basketPrice">${item.price}</div>
 
                 <div className="basketDelet basketButton">
-                  <button type="button"
-                  onClick={() => deleteItem(item.product)} > X </button>
+                  <button
+                    type="button"
+                    onClick={() => deleteItem(item.product)}
+                  >
+                    X
+                  </button>
                 </div>
               </li>
-             
             ))
-          }
+          )}
           <li>
             <div className="continueShopping">
               <Link to="/">&#11013;</Link>
-              <Link to={"/"} >Continue Shopping</Link>
+              <Link to={"/"}>Continue Shopping</Link>
             </div>
-            
-            
+
             <div>
-            <span className="subtotal">Subtotal: </span>
-            <span  className="total">${basketItems.reduce((x, y) => x + y.price * y.quantaty, 0)}</span>
+              <span className="subtotal">Subtotal: </span>
+              <span className="total">
+                ${basketItems.reduce((x, y) => x + y.price * y.quantaty, 0)}
+              </span>
             </div>
           </li>
         </ul>
       </div>
 
       <div className="basketAction">
-      
-      <ul className="actionContainer">
-        <li>
-        <b>Number of items: </b>     
-        {basketItems.reduce((x, y) => x + y.quantaty, 0)} items 
-        </li>
-        <li>
-          <b>Item Sub-Total: </b>
-           {basketItems.reduce((x, y) => x + y.price * y.quantaty, 0)} 
-        </li>
-        <li>
-          <b>Shipping: </b>
-           <i> FREE</i> 
-        </li>
-        <li className="basketTotal">
-          <b>Total:</b>
-           {basketItems.reduce((x, y) => x + y.price * y.quantaty, 0)} 
-        </li>
-      </ul>
-        
-       
-    
+        <ul className="actionContainer">
+          <li>
+            <b>Number of items: </b>
+            {basketItems.reduce((x, y) => x + y.quantaty, 0)} items
+          </li>
+          <li>
+            <b>Item Sub-Total: </b>
+            {basketItems.reduce((x, y) => x + y.price * y.quantaty, 0)}
+          </li>
+          <li>
+            <b>Shipping: </b>
+            <i> FREE</i>
+          </li>
+          <li className="basketTotal">
+            <b>Total:</b>
+            {basketItems.reduce((x, y) => x + y.price * y.quantaty, 0)}
+          </li>
+        </ul>
+
         <button
           disabled={basketItems.length === 0}
           onClick={checkout}
           className="checkout"
         >
-          
           Checkout
         </button>
       </div>
