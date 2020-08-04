98c06f089a3284e733b92e0dcfad511f7e6ccec8
samirazazy <samiralazazy@gmail.com>
2020-08-04 12:29:04 +0200

    deployment configurations

--- nodejs/server.js
+++ nodejs/server.js
@@ -7,25 +7,17 @@ app.get("/api/items/:id", (req, res) => {
   const itemId = req.params.id;
   const item = data.items.find((el) => el._id === itemId);
 
-  if (item) {
-    res.send(item);
-    // console.log(item);
-  } else {
-    res.status(404).send({ msg: "Item is not found..." });
-    // console.log(msg);
-  }
+  item ?
+    res.send(item) :
+    res.status(404).send({
+      msg: "Item is not found..."
+    })
 });
 
-app.get("/api/items", (req, res) => {
-  res.send(data.items);
-});
+app.get("/api/items", (req, res) => res.send(data.items));
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static("reactjs/build"));
-}
+process.env.NODE_ENV == "production" && app.use(express.static("reactjs/build"))
 
-app.listen(PORT, () => {
-  console.log(`server started successfully at ${PORT} `);
-});
+app.listen(PORT, () => console.log(`server started successfully at ${PORT} `))
